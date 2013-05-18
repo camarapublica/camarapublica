e341a6f3f5bb18aca1b49d3dcee94d6633ff0d1d
José Tomás Albornoz <jojo@eljojo.net>
2013-05-18 02:58:04 +0200

    Added "configure_permitted_parameters" in ApplicationController so devise works with strong_parameters.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) do |u| 
+      u.permit(:name, :email, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.for(:account_update) do |u| 
+      u.permit(:name, :email, :password, :password_confirmation, :current_password)
+    end
+  end
 end
